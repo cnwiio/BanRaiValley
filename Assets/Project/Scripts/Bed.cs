9b9ff078a3e8443401f72810a1317a9be7a831c3
cnwiio <kritgoodgame@gmail.com>
2026-08-25 20:13:08 +0700

    Prototype 0.6.3
    Add outline material
    make bed show outline when hover

--- Assets/Project/Scripts/Bed.cs
+++ Assets/Project/Scripts/Bed.cs
@@ -1,16 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 public class Bed : MonoBehaviour, IInteractable
 {
+    [Header("UI")]
     [SerializeField] private TextMeshProUGUI tipTextUI;
     [SerializeField] private String tipText;
+    [Header("Outline")]
+    [SerializeField] private MeshRenderer meshRenderer;
+    [SerializeField] private Material outlineMaterial;
 
     private GameObject uiGameObject;
+    private List<Material> mats = new List<Material>();
     private void Awake()
     {
         uiGameObject = tipTextUI.gameObject;
+        meshRenderer.GetSharedMaterials(mats);
     }
 
     public void Interact()
@@ -35,10 +42,16 @@ public class Bed : MonoBehaviour, IInteractable
     {
         tipTextUI.text = tipText;
         uiGameObject.SetActive(true);
+        
+        mats.Add(outlineMaterial);
+        meshRenderer.SetSharedMaterials(mats);
     }
 
     public void OnStopHover()
     {
         uiGameObject.SetActive(false);
+
+        mats.Remove(outlineMaterial);
+        meshRenderer.SetSharedMaterials(mats);
     }
 }
